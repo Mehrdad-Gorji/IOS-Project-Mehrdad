c7815eff0398544a3bb93417dc214853ca11998f
mehrdadgorji <mehrdadgorji@gmail.com>
2024-10-20 00:23:32 +0200

    Initial Commit

---
+++ IOS-Project-Mehrdad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOS-Project-Mehrdad
+//
+//  Created by Mehrdad Gorji on 20.10.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOS-Project-Mehrdad/IOS_Project_MehrdadApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Project_MehrdadApp.swift
+//  IOS-Project-Mehrdad
+//
+//  Created by Mehrdad Gorji on 20.10.24.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Project_MehrdadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
