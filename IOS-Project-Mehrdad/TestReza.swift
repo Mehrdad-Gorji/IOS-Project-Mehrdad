02d66420f808f9a82ab735b54e0de1b8af965916
Rztajalli <rztajalli@gmail.com>
2024-10-21 11:59:29 +0200

    rezaist

--- IOS-Project-Mehrdad/TestReza.swift
+++ IOS-Project-Mehrdad/TestReza.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct TestReza: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Reza ist hier")
     }
 }
 
