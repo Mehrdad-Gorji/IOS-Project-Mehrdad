ca44b4b938b3ede72d82db188a59ea26740c1566
Rztajalli <rztajalli@gmail.com>
2024-10-21 11:39:26 +0200

    Login

---
+++ IOS-Project-Mehrdad/ViewModels/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  IOS-Project-Mehrdad
+//
+//  Created by Reza.tajalli on 21.10.24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
