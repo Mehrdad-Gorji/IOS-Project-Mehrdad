9b83f8eee1f7aa95a1e81644a18e695f756c3e95
Rztajalli <rztajalli@gmail.com>
2024-10-21 11:42:09 +0200

    test reza

---
+++ IOS-Project-Mehrdad/TestReza.swift
@@ -0,0 +1,18 @@
+//
+//  TestReza.swift
+//  IOS-Project-Mehrdad
+//
+//  Created by Reza.tajalli on 21.10.24.
+//
+
+import SwiftUI
+
+struct TestReza: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestReza()
+}
